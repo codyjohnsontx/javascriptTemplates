700b74647bbd4264df47746c3b622b69d355de2e
Cody Johnson <codyjohnsontx@gmail.com>
2024-03-07 16:20:38 -0600

    add class constructor template

---
+++ objectConstructorClass.js
@@ -0,0 +1,16 @@
+class MakeCar {
+  constructor(carMake, carModel, carColor, numOfDoors) {
+    this.make = carMake;
+    this.model = carModel;
+    this.color = carColor;
+    this.doors = numOfDoors;
+  }
+  honk() {
+    alert("BEEP BEEP FUCKER");
+  }
+  lock() {
+    alert(`Locked ${this.doors} doors!`);
+  }
+}
+
+let hondaCivic = new MakeCar("Honda", "Civic", "Silver", 4);
